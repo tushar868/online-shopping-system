ba67f909744a0fca5263e0bf394b52531097dd62
Tushar Lohar <65663535+tushar868@users.noreply.github.com>
2022-07-16 19:08:22 +0530

    Update footer.php

--- admin/footer.php
+++ admin/footer.php
@@ -4,7 +4,7 @@
             <ul>
               <li>
                 <a href="">
-                  PuneethReddy
+                  
                 </a>
               </li>
             </ul>
@@ -13,8 +13,8 @@
             &copy;
             <script>
               document.write(new Date().getFullYear())
-            </script>, made with <i class="material-icons">favorite</i> by
-            <a href="" target="_blank">Puneethreddy</a> for a better web.
+            </script>, made with <i class="material-icons"></i> 
+            
           </div>
           <!-- your footer here -->
         </div>
